4b15220c12bf92f7eb75ac0c27a2a3357d853195
Sergio Zanucci <sergiozanu@gmail.com>
2024-03-13 16:58:21 -0300

    Painel de senha com madal, em processo de finalização, HTML,CSS,JS

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector(".modal__header")
+
+function abreModal() {
+    let modalHeader = document.querySelector(".modal__header");
+    modalHeader.style.display = "block";
+}
+
+document.querySelector(".modal__header");
+
+function fecharModal() {
+   let cancelar = document.querySelector(".modal__header");
+    cancelar.style.display = "none";
+     
+}
+   
+
+
+    
+
+
+   
+
+
