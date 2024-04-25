5aa420438d1774907d2f0d482a66668421e4bf80
Sergio Zanucci <sergiozanu@gmail.com>
2024-04-25 13:44:41 -0300

    Atualização, reduzindo numeros par cnpj e cpf

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 function abreModal() {
     let modalHeader = document.querySelector(".modal__header");
     modalHeader.style.display = "block";
+    modalHeader = document.querySelector(".container__numerico");
+    modalHeader.style.display = "block";
 }
 
 // Fecha modal atraves do botão cancelar.
@@ -17,19 +19,19 @@ function fecharModal() {
 }
 
 // Funcão inserir numeros atraves do teclado numerico.
-
 function inserirNumero(num) {
     let btnNumerico = document.querySelector("input");
-    btnNumerico.value += num;
+    if (btnNumerico.value.length < 14) {
+        btnNumerico.value += num;
+    }
 }
-
 // Função fechar container numerico, atraves do botão ok
 
 document.querySelector(".container__numerico");
 
 function btnOk() {
     let containerNumerico = document.querySelector(".container__numerico");
-    // containerNumerico.style.display = "none";
+    containerNumerico.style.display = "none";
 }
 
 // Função limpar campo do input atraves do botão limpar
